11b3a69d348fd79a0076531a8c104783d983af9e
yunior123 <yuniorrodriguezo460@gmail.com>
2022-01-15 20:12:54 -0500

    Update main.dart

--- flutter_awesome_loading_button/example/lib/main.dart
+++ flutter_awesome_loading_button/example/lib/main.dart
@@ -14,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Awesome Loading Button Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Awesome Loading Button IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Awesome Loading Button Demo Home Page'),
@@ -32,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -61,6 +43,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: AwesomeLoadingButton(
+          loadingIndicatorColor: Colors.blue,
+          loadingIndicatorValueColor: const AlwaysStoppedAnimation(
+            Colors.blueAccent,
+          ),
           onPressed: () async {
             await Future.delayed(
               const Duration(
